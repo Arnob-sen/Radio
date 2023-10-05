bb1da852acbff2d2436d3807e645714a4d55f3b2
Arnob-sen <arnobsen16.2@gmail.com>
2023-10-05 18:11:13 +0600

    change_class in_functional_and_add_text_to_speech

--- src/App.js
+++ src/App.js
@@ -1,67 +1,76 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
+import { useSpeechSynthesis } from 'react-speech-kit';
 
-class  App  extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      stations: [
-      {name:'USA',number:'USA alert'},
-      {name:'Canada',number:'Can alert'},
-      {name:'India',number:'Ind alert'},
-      {name:'Bangladesh',number:'Ban alert'}
-      ],
-      current: null
-    }
+function App() {
+  const [stations, setStations] = useState([
+    {name:'USA'},
+    {name:'Canada'},
+    {name:'India'},
+    {name:'Bangladesh'}
+  ]);
+  const [current, setCurrent] = useState(null);
+  const [text,setText]=useState('');
+  const {speak}=useSpeechSynthesis();
+  const handleClick=()=>{
+    speak({text:text})
   }
-  render() {
-    const { current, stations } = this.state;
-      return (
-        <div>
-          <div className="nav">
-            <div className="nav-item"></div>
-            <div className="nav-item">Stream</div>
-            <div className="nav-item"></div>
-          </div>
-          <ul className="station-list">
-            {stations.map((station, key) => {
-              return <Station
-              key={key}
-              station={station}
-              current={current}
-              onStationSelect={ current => this.setState({current}) } />
-            })}
-          </ul>
-          <div className="station-info">
-            <div className="header">{current ? 'CURRENTLY PLAYING' : ''}</div>
-            <div className="header-name">{current ? current.name : ''}</div>
-          </div>
-        </div>
-      );
-  }
+
+  const handleStationSelect = (selectedStation) => {
+    setCurrent(selectedStation);
+  };
+
+  return (
+    <div>
+      <div className="nav">
+        <div className="nav-item"></div>
+        <div className="nav-item">Stream</div>
+        <div className="nav-item"></div>
+      </div>
+      <ul className="station-list">
+        {stations.map((station, key) => (
+          <Station
+            key={key}
+            station={station}
+            current={current}
+            onStationSelect={handleStationSelect}
+          />
+        ))}
+      </ul>
+      <div className="station-info">
+        <div className="header">{current ? 'CURRENTLY PLAYING' : ''}</div>
+        <div className="header-name">{current ? current.name : ''}</div>
+      </div>
+      <input type='text' onChange={(e)=>{setText(e.target.value)}}/>
+      <button onClick={()=>handleClick()} >Speak</button>
+    </div>
+  );
 }
 
-const Station = (props) => {
-    const { station, onStationSelect, current } = props;
-    return (
-      <div className="station" onClick={() => onStationSelect(station)}>
-        { (station === current) ? <StationDetails
-        current={current}/> : '' }
-        <span className="station-name">{station.name}</span>
-        {/* <span className="station-number">{station.number}</span> */}
-      </div>
-    )
+function Station(props) {
+  const { station, onStationSelect, current } = props;
+
+  return (
+    <div className="station" onClick={() => onStationSelect(station)}>
+      {station === current ? <StationDetails /> : ''}
+      <span className="station-name">{station.name}</span>
+      <span className="station-number">{station.number}</span>
+    </div>
+  );
 }
 
-const StationDetails = (props) => {
-  const{current}=props;
-    return (
-      <div className="station-details">
-        <div className="station-details-item"></div>
-        <div className="station-details-item"></div>
-        <div className="station-details-item"></div>
-      </div>
-    )
+function StationDetails(props) {
+  return (
+    <div className="station-details">
+      <div className="station-details-item"></div>
+      <div className="station-details-item"></div>
+      <div className="station-details-item"></div>
+    </div>
+  );
 }
+
+
+
 export default App;
